0e0046e8ebd6f774468ae6ab095834c7eb64e43a
Sheprut Ilya <optitel223@gmail.com>
2018-10-26 23:41:00 +0700

    Добавлены примеры для кода и графиков по данным

---
+++ examples/code/a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+//-----------------------------------------------------------------------------
+
+int main(void) {
+	cout << "Hello World" << endl;
+	// very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong line
+	// Example of math comment: $A_i^{m+n+k+1} = \sum_{i=1}^{n} i^{-i}$
+	return 0;
+}
---
+++ examples/graph_by_data/README.md
@@ -0,0 +1,11 @@
+# Построение графиков по данным
+
+Такая задача часто встречается при создании отчетов.
+
+Данные разделяются табами. В первой строке находится название каждого столбца.
+
+Документация по пакету [tikz](http://ctan.altspu.ru/graphics/pgf/base/doc/pgfmanual.pdf) - пакет для создания графиков и фигур.
+
+Документация по пакету [pgfplots](https://www.iro.umontreal.ca/~simardr/pgfplots.pdf) - пакет для создания графиков.
+
+Документация по пакету [pgfplotstable](http://pgfplots.sourceforge.net/pgfplotstable.pdf) - пакет для ввода таблиц.
